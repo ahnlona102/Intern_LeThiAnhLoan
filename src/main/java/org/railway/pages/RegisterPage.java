8020139099b8d778916a941ffbac2b688e1796c6
ahnlona102 <anhloanlemt1@gmail.com>
2024-06-10 17:20:32 +0700

    Implement Testcase

--- src/main/java/org/railway/pages/RegisterPage.java
+++ src/main/java/org/railway/pages/RegisterPage.java
@@ -1,10 +1,12 @@
 package org.railway.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WindowType;
 import org.railway.utils.Action;
 import org.railway.utils.Driver;
 
 import java.time.Duration;
+import java.util.Properties;
 
 public class RegisterPage {
 
@@ -21,30 +23,34 @@ public class RegisterPage {
     private final By confirmPass = By.id("confirmPassword");
     private final By passporttxtbox = By.name("pid");
     private final By registerbtn = By.cssSelector("p.form-actions>input");
+    protected static Properties properties;
+
 
     public void getEmail(String username, String domain) {
-        Driver.driver.get("https://www.guerrillamail.com/inbox");
+        Driver.driver.get(properties.getProperty("mailUrl"));
         Action.click(Action.find(usernametxtbox));
         Action.sendKeys(Action.find(usernameinput), username);
         Action.selectByValue(Action.find(domainselect), domain);
         Action.click(Action.find(setbtn));
         Action.click(Action.find(uncheckbox));
-        String email = Action.getText(Action.find(emailField));
-        System.out.println("Get Email:" + email);
+        String getemail = Action.getText(Action.find(emailField));
+        System.out.println("Get Email:" + getemail);
     }
-
     public void RegisterAccount(String email, String password, String confirmpassword, String passport) {
+        Driver.driver.switchTo().newWindow(WindowType.TAB);
+        Driver.driver.get(properties.getProperty("url"));
+        Driver.driver.navigate().refresh();
         Action.sendKeys(Action.find(emailtxtbox), email);
         Action.sendKeys(Action.find(passwordtxtbox), password);
         Action.sendKeys(Action.find(confirmPass), confirmpassword);
         Action.sendKeys(Action.find(passporttxtbox), passport);
         Action.click(Action.find(registerbtn));
     }
-
-    public void confirmEmail(String email) {
-        Driver.driver.get("https://www.guerrillamail.com/inbox");
-        Driver.driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+    public void confirmEmail() {
+        Driver.driver.switchTo().window(Driver.driver.getWindowHandles().iterator().next());
+        Driver.driver.navigate().refresh();
         Action.click(Action.find(confirmMess));
         Action.click(Action.find(confirmLink));
     }
+
 }
